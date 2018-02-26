531d7e02c6e586d4eea96b2f89919013135b6229
melainesit <melainesit@gmail.com>
2018-02-26 15:30:53 -0800

    REAL unit 6 turnin

---
+++ unit6project/src/Lab08g.java
@@ -0,0 +1,26 @@
+//© A+ Computer Science  -  www.apluscompsci.com
+//Name -
+//Date -
+//Class -
+//Lab  -
+import static java.lang.System.*;
+
+public class Lab08g
+{
+	public static void main ( String[] args )
+	{
+		//add test cases
+		LoopStats pan = new LoopStats();
+		pan.setNums(1,5);
+		pan.getEvenCount();
+		pan.getOddCount();
+		pan.getTotal();
+		System.out.println(pan.toString());
+		
+		
+		
+		
+		
+					
+	}
+}
---
+++ unit6project/src/Lab09f.java
@@ -0,0 +1,21 @@
+//© A+ Computer Science  -  www.apluscompsci.com
+//Name -
+//Date -
+//Class -
+//Lab  -
+import static java.lang.System.*;
+
+public class Lab09f
+{
+	public static void main( String args[] )
+	{
+		//add test cases
+		LetterRemover pan = new LetterRemover();
+		pan.setRemover("I am Sam I am", 'a');
+		pan.removeLetters();
+		System.out.println(pan.toString());
+		
+		
+											
+	}
+}
